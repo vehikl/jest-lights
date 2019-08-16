f0ae2fee22263ccab372c66a0ed4cd439362dabf
Vehikl Team <go@vehikl.com>
2019-08-16 16:42:24 -0400

    Fix yellow light and turn red on run-time errors

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const axios = require('axios');
+const axios = require('axios')
 
 module.exports = class {
 
-  constructor() {
-    this.token = null;
+  constructor () {
+    this.token = null
+    axios.post('http://localhost:8080/light/yellow/only')
   }
-  // Add hooks to Jest lifecycle events
-  apply(jestHooks) {
-    jestHooks.onTestRunComplete((results) => {
-      if (results.numFailedTests > 0) {
-        console.log('failed')
-        return axios.post('http://localhost:8080/light/red/only');
-      }
-      console.log('pass')
-      return axios.post('http://localhost:8080/light/green/only');
-    });
 
-    jestHooks.shouldRunTestSuite(() => {
-      return axios.post('http://localhost:8080/light/yellow/only').then(() => true);
-    });
+  apply (jestHooks) {
+    jestHooks.onTestRunComplete((results) => {
+      if (!results.success) {
+        return axios.post('http://localhost:8080/light/red/only')
+      }
+
+      return axios.post('http://localhost:8080/light/green/only')
+    })
+
+    jestHooks.onFileChange(() => {
+      axios.post('http://localhost:8080/light/yellow/only')
+    })
   }
 }
