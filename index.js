0816cff48d4b0e4b6ff09d21209d426b2425b97f
Alex Barry <alex.barry@gmail.com>
2019-08-19 13:57:10 -0400

    API Updates
     - Support sending params to API
     - Add fallback logging if lights unreachable

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const api = require('./lib/api.js');
 
-const delay = ms => new Promise((resolve) => {
-  setTimeout(resolve, ms);
-});
-
 module.exports = class JestLightsReporter {
   onRunStart(_results, _options) {
     return api.yellow();
   }
 
   onRunComplete(_contexts, results) {
-    const resultLight = results.success
-      ? api.green()
-      : api.red();
-
-    return resultLight
-      .then(() => delay(5000))
-      .then(() => api.off());
+    return (
+      results.success
+        ? api.green()
+        : api.red()
+    )
+      .then(() => api.off({ delay: 5000 }));
   }
 }
